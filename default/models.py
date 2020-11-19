6b5cbbd5590663b84fe9a93b338dda2c0b4e3ee1
Apollo.Yu <10930324@dcsh.tp.edu.tw>
2020-11-19 11:43:24 +0800

    投票主題新增，修改，刪除

--- default/models.py
+++ default/models.py
@@ -3,8 +3,8 @@ from django.db import models
 # Create your models here.
 class Poll(models.Model):
     #投票主題文字，至多 200 字
-    subject = models.CharField(max_length=255)
-    description =models.TextField()
+    subject = models.CharField("主題投票", max_length=255)
+    description =models.TextField("投票內容說明")
     # 投票建立日期，在建立時若未指定，則自動填入建立時的時間
     date_created = models.DateField(auto_now_add=True)
 
@@ -13,10 +13,12 @@ class Poll(models.Model):
 
 class Option(models.Model):
     # 此選項屬於哪一個投票
-    poll_id = models.IntegerField()
+    poll_id = models.IntegerField("所需投票主題編號")
     # 選項文字
-    title = models.CharField(max_length=200)
+    title = models.CharField("選項標題", max_length=200)
     # 此選項被投票數
     count = models.IntegerField(default=0)
 
+    def __str__(self):
+        return str(self.id) + ")" + self.subject
     
--- default/views.py
+++ default/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import Poll, Option
-from django.views.generic import ListView, DetailView, RedirectView
+from django.views.generic import  *
 
 # Create your views here.
 # def poll_list(req):
@@ -27,3 +27,19 @@ class PollVote(RedirectView):
         option.save()       # 儲存至資料庫
         return "/poll/{}/".format(option.poll_id)
         #return "/poll/"+str(option.poll_id)+'/'
+
+class PollCreate(CreateView):
+    model = Poll
+    fields = '__all__'    #['subject', 'description']
+    success_url = '/poll/'
+
+class PollEdit(UpdateView):
+    model = Poll
+    fields = '__all__'
+    success_url = '/poll/'
+
+class PollDelete(DeleteView):
+    model = Poll
+    success_url = '/poll/'
+
+
